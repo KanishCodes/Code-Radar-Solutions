19fb5f67ba2db7ca6fd6baefb1c75b8cc69ac666
Kanish <ssdnkanishmahajan@gmail.com>
2025-03-27 19:15:06 +0530

    Solution to Print Multiplication Table

---
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N); // Input the size of the triangle
+    for (int i = 1; i <= N; i++) {
+        printf("%d x %d = %d\n", N, i, i*N);
+        
+    }
+
+    return 0;
+}
