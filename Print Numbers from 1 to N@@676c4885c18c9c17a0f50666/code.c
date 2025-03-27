998632568203e43271b8dd6ae43aff819ec82808
Kanish <ssdnkanishmahajan@gmail.com>
2025-03-27 19:12:19 +0530

    Solution to Print Numbers from 1 to N

---
+++ Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N); // Input the size of the triangle
+
+    for (int i = 1; i <= N; i++) {
+        printf("%d ",i);
+    }
+
+    return 0;
+}
