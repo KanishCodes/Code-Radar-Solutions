7c43a0245423cf8a3fab6dce4b6464fa138c7a44
Kanish <ssdnkanishmahajan@gmail.com>
2025-03-27 18:55:53 +0530

    Solution to Print a Pyramid Pattern with Numbers

---
+++ Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N); // Input the size of the triangle
+    for(int i = 1;i<=N; i++){
+        for(int j = 1; j<=N-i;j++){
+            printf(" ");
+        }
+        for(int k = 1; k<=i; k++){
+            printf("%d ", k);
+        }
+        
+    }
+
+    return 0;
+}
