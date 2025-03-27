a99f40c88943c34d0fdddd504b3cff8098f3db5c
Kanish <ssdnkanishmahajan@gmail.com>
2025-03-27 19:20:08 +0530

    Solution to Insertion Sort for Integers

---
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -0,0 +1,19 @@
+int insertionSort(int arr[], int a){
+    for (int i = 0; i < a - 1; i++) {  // Number of passes (each pass places the largest element at the end)
+        for (int j = 0; j < a - i - 1; j++) {  // Comparing adjacent elements
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1] to move the larger element forward
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+}
+}
+
+
+int printArray(int arr[], int x){
+    for(int i = 0; i<=x;i++){
+        printf("%d ", arr[i]);
+    }
+}
